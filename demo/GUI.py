3369b71c0b0a502e838833056d7e06d39d0ffd32
Kiara Vicencio <kiarabvicencio@su.edu.ph>
2023-03-27 13:38:13 +0800

    only shows metadata of the folder & not each file

--- demo/GUI.py
+++ demo/GUI.py
@@ -1,77 +1,82 @@
 import os
-import PySimpleGUI as sg
-from PIL import Image
-import os.path
-from PIL.ExifTags import TAGS
+import tkinter as tk
+from tkinter import filedialog
 
-# function to get image metadata
-def get_image_metadata(filename):
-    with Image.open(filename) as img:
-        metadata = img._getexif()
-    return metadata
+# Function to display folder contents and metadata
+def display_folder():
+    # Clear any previous contents from the listbox and metadata label
+    listbox.delete(0, tk.END)
+    metadata_label.config(text='')
 
-# define GUI layout
-layout = [
-    [sg.Text("Select a folder:"),
-    sg.FolderBrowse(key="-FOLDER-")],
-    [sg.Text("Files in folder:")],
-    [sg.Column([
-        [sg.Listbox(values=[], size=(70, 30), key="-FILE LIST-", enable_events=True)]
-    ]),
-    sg.VSeperator(),
-    sg.Column([
-        [sg.Text("Selected image metadata:")],
-        [sg.Text(size=(20, 1), key="-METADATA-")],
-    ])],
-    [sg.Button("Open new window")]
-]
+    # Get the selected folder from the user
+    folder_path = filedialog.askdirectory()
 
+    # Get the contents of the folder and add them to the listbox
+    folder_contents = os.listdir(folder_path)
+    for item in folder_contents:
+        listbox.insert(tk.END, item)
 
-# create the GUI window
-window = sg.Window("Image Metadata Viewer", layout)
+    # Get the metadata for the folder and display it
+    folder_metadata = os.stat(folder_path)
+    metadata_str = f'Size: {folder_metadata.st_size} bytes\nModified: {folder_metadata.st_mtime}'
+    metadata_label.config(text=metadata_str)
 
-while True:
-    event, values = window.read()
-    if event == "Exit" or event == sg.WIN_CLOSED:
-        break
-    # Folder name was filled in, make a list of files in the folder
-    if event == "-FOLDER-":
-        folder = values["-FOLDER-"]
-        try:
-            # Get list of files in folder
-            file_list = os.listdir(folder)
-        except:
-            file_list = []
+# Function to open a new window with the same functionalities
+def open_new_window():
+    # Create a new window
+    new_window = tk.Toplevel(root)
+    new_window.geometry("500x500")
 
-        fnames = [
-            f
-            for f in file_list
-            if os.path.isfile(os.path.join(folder, f))
-            
-        ]
-        window["-FILE LIST-"].update(fnames)
+    # Create a listbox and metadata label for the new window
+    listbox_new = tk.Listbox(new_window)
+    metadata_label_new = tk.Label(new_window)
 
-    # display metadata when image is selected
-    if event == "-FILE LIST-":
-        filename = os.path.join(values["-FOLDER-"], values["-FILE LIST-"][0])
-        metadata = get_image_metadata(filename)
-        window["-METADATA-"].update(str(metadata))
+    # Add the listbox and metadata label to the new window
+    listbox_new.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+    metadata_label_new.pack()
 
-    # open a new window with the same features
-    if event == "Open new window":
-        new_window = sg.Window("Image Metadata Viewer", layout)
-        while True:
-            new_event, new_values = new_window.read()
-            if new_event == sg.WIN_CLOSED or new_event == "Exit":
-                break
-            if new_event == "-FOLDER-":
-                folder = new_values["-FOLDER-"]
-                fnames = [f for f in os.listdir(folder)]
-                new_window["-FILE LIST-"].update(fnames)
-            if new_event == "-FILE LIST-":
-                filename = os.path.join(new_values["-FOLDER-"], new_values["-FILE LIST-"][0])
-                metadata = get_image_metadata(filename)
-                new_window["-METADATA-"].update(str(metadata))
-        new_window.close()
+    # Create a button to select a folder in the new window
+    select_folder_button_new = tk.Button(new_window, text='Select Folder', command=lambda: display_folder_new(listbox_new, metadata_label_new))
+    select_folder_button_new.pack(side=tk.TOP, padx=10, pady=10)
 
-window.close()
+# Function to display folder contents and metadata in the new window
+def display_folder_new(listbox_new, metadata_label_new):
+    # Clear any previous contents from the listbox and metadata label
+    listbox_new.delete(0, tk.END)
+    metadata_label_new.config(text='')
+
+    # Get the selected folder from the user
+    folder_path = filedialog.askdirectory()
+
+    # Get the contents of the folder and add them to the listbox
+    folder_contents = os.listdir(folder_path)
+    for item in folder_contents:
+        listbox_new.insert(tk.END, item)
+
+    # Get the metadata for the folder and display it
+    folder_metadata = os.stat(folder_path)
+    metadata_str = f'Size: {folder_metadata.st_size} bytes\nModified: {folder_metadata.st_mtime}'
+    metadata_label_new.config(text=metadata_str)
+
+# Create the main window
+root = tk.Tk()
+root.geometry("500x500")
+
+# Create a listbox and metadata label for the main window
+listbox = tk.Listbox(root)
+metadata_label = tk.Label(root)
+
+# Add the listbox and metadata label to the main window
+listbox.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+metadata_label.pack()
+
+# Create a button to select a folder in the main window
+select_folder_button = tk.Button(root, text='Select Folder', command=display_folder)
+select_folder_button.pack(side=tk.TOP, padx=5, pady=5)
+
+# Create a button to open a new window with the same functionalities
+new_window_button = tk.Button(root, text='Open New Window', command=open_new_window)
+new_window_button.pack(side=tk.BOTTOM, padx=10, pady=10)
+
+# Start the main loop
+root.mainloop()
